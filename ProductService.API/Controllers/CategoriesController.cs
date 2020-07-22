a6bad27a48d0eea25be70ef2b317b8cd56d6354f
Vasil Indzhev <v.indzhev@gmail.com>
2020-07-22 13:50:49 +0300

    added dummy category endpoint

---
+++ ProductService.API/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+﻿namespace ProductService.API.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using MicroservicesPOC.Shared.API.Controllers;
+    using System.Threading.Tasks;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : ApiController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+            await Task.Delay(50);
+            return this.Ok(new string[] { "house", "vehicle", "family", "travel", "work", "pet" });
+        }
+    }
+}
